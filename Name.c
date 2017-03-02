34bf737234ee12c4ab97c2b02092711bf48ab587
srilakshmii <610sssg@gmail.com>
2017-03-02 11:03:08 +0530

    Update Name.c

--- Name.c
+++ Name.c
@@ -1,8 +1,8 @@
 //EVERYONE WRITE A STATEMENT TO PRINT YOUR NAME
 printf("Devan");
 printf("shaun");
-printf("sterin");
 printf("tana");
 printf("megha");
 printf("Vandana");
 printf("tino");
+printf("srilakshmi");
